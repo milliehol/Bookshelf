e8f883d42109ed77aae4cf6617926d6bfab73df1
Carla <carla.programmer@gmail.com>
2020-07-17 17:13:55 -0400

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,4 +2,6 @@ document.addEventListener('DOMContentLoaded', () => {
   const app = new App();
   app.attachEventListeners();
   app.adapter.fetchAuthors().then(app.createAuthors);
+  app.adapter.fetchBooks().then(app.createBooks);
+
 });
