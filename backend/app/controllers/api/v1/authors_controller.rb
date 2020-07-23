c12d273cffdf7319fbcdb417151e38678e582d44
Carla <carla.programmer@gmail.com>
2020-07-22 21:51:51 -0400

    added ability to update author.

--- backend/app/controllers/api/v1/authors_controller.rb
+++ backend/app/controllers/api/v1/authors_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::AuthorsController < ApplicationController
 
 
-def index
+ def index
    @authors = Author.all
    render json: @authors
  end
@@ -9,6 +9,16 @@ def index
  def create
    @author = Author.create(author_params)
    render json: @author
+ end
+
+ def update
+   @author = Author.find_by_id(params[:id])
+
+   if @author.save
+     render json: @author
+   else
+     render json: {errors: @author.errors.full_messages}, status: 422
+   end
  end
 
 
